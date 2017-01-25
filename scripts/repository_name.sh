c0beb0e0a20064d155f68c29af25cf736133d986
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-26 01:28:51 +0900

    Implement repository_name.sh

--- scripts/repository_name.sh
+++ scripts/repository_name.sh
@@ -4,6 +4,22 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/helpers.sh"
 
+repository_name() {
+  repo_name="$(
+    git config --get remote.origin.url \
+      | grep github.com \
+      | sed 's/^.*github\.com[:\/]\(.*\)\.git$/\1/' \
+      | tr -d '\n'
+  )"
+
+  if [ -n "$repo_name" ]; then
+    printf $repo_name
+  else
+    printf `basename $SHELL`
+  fi
+}
+
 main() {
+  repository_name
 }
 main
