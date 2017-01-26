06f5601c03749edc31e75c6a12ea896077437b55
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-26 09:45:41 +0900

    Use show-window-option insted of show-option

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,7 +1,7 @@
-get_tmux_option() {
+get_tmux_window_option() {
   local option="$1"
   local default_value="$2"
-  local option_value="$(tmux show-option -gqv "$option")"
+  local option_value="$(tmux show-window-option -gv "$option")"
   if [ -z "$option_value" ]; then
     echo "$default_value"
   else
