b3f5aad6a3ea9c339dfddda416e338ee30276311
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2017-01-27 22:44:53 +0900

    Implement tmux_rename_window function

--- functions/tmux_rename_window.fish
+++ functions/tmux_rename_window.fish
@@ -1,2 +1,5 @@
-function tmux_rename_window -d "TODO"
+function tmux_rename_window -e fish_prompt -v PWD
+    if tmux info > /dev/null ^ /dev/null
+        tmux rename-window (github_repository_name)
+    end
 end
