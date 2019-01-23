ed13ec74eac7a3d7070577e6dd5b95ec4189f544
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2019-01-23 19:49:10 +0900

    Enable renaming when resuming the process

--- conf.d/tmux.fish
+++ conf.d/tmux.fish
@@ -3,6 +3,6 @@ if not command -s tmux > /dev/null
     exit 1
 end
 
-function __tmux_rename_window_on_hook -e fish_prompt -v PWD
+function __tmux_rename_window_on_hook -e fish_prompt -s CONT -v PWD
     __tmux_rename_window
 end
